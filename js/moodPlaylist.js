56e92f49416c65055e552c930e4377c92afb2ef8
phianova <98988580+phianova@users.noreply.github.com>
2023-11-09 17:05:56 +0000

    Mood based playlist selector
    Added buttons to select your mood within a modal and an iframe to display a tailored playlist. Initially tried to determine the link to be embedded via JS but then realised it could be done via href= and target= right in the HTML

--- js/moodPlaylist.js
+++ js/moodPlaylist.js
@@ -1,19 +1,33 @@
+//Wait til content loaded
 document.addEventListener("DOMContentLoaded", function() {
 
+//Declarations
     let modalContent = document.querySelector(".modal-content");
     let openModal = document.querySelector(".open-modal");
     let closeModal = document.querySelector(".close-modal");
     let blurBackground = document.querySelector(".blur-background");
+    let moodBox = document.querySelector(".moods-box");
+    let embeddedPlaylist = document.querySelector(".embedded-playlist");
+    let moodSelector1 = document.querySelector(".mood-selector-1");
+    let moodSelector2 = document.querySelector(".mood-selector-2");
+    let moodSelector3 = document.querySelector(".mood-selector-3");
+    let moodSelector4 = document.querySelector(".mood-selector-4");
+    let moodSelector5 = document.querySelector(".mood-selector-5");
+    let moodSelector6 = document.querySelector(".mood-selector-6");
+    let moodSelector7 = document.querySelector(".mood-selector-7");
 
+// Control modal opening, closing and background blurring
     openModal.addEventListener("click", function() {
         modalContent.classList.remove("hidden-modal");
         blurBackground.classList.remove("hidden-blur");
+        moodBox.classList.remove("hidden-box");
+        embeddedPlaylist.classList.add("hidden-playlist");
     });
 
     let closeModalFunction = function() {
         modalContent.classList.add("hidden-modal");
         blurBackground.classList.add("hidden-blur");
-    }
+    };
 
     blurBackground.addEventListener("click", closeModalFunction);
     closeModal.addEventListener("click", closeModalFunction);
@@ -24,6 +38,45 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+//Control mood selector and playlists - NOW UNNECESSARY!
+
+    moodSelector1.addEventListener("click", function() {
+        embeddedPlaylist.classList.remove("hidden-playlist");
+        moodBox.classList.add("hidden-box");
+    });
+
+    moodSelector2.addEventListener("click", function() {
+        embeddedPlaylist.classList.remove("hidden-playlist");
+        moodBox.classList.add("hidden-box");
+    });
+
+    moodSelector3.addEventListener("click", function() {
+        embeddedPlaylist.classList.remove("hidden-playlist");
+        moodBox.classList.add("hidden-box");
+    });
+
+    moodSelector4.addEventListener("click", function() {
+        embeddedPlaylist.classList.remove("hidden-playlist");
+        moodBox.classList.add("hidden-box");
+    });
+
+    moodSelector5.addEventListener("click", function() {
+        embeddedPlaylist.classList.remove("hidden-playlist");
+        moodBox.classList.add("hidden-box");
+    });
+
+    moodSelector6.addEventListener("click", function() {
+        embeddedPlaylist.classList.remove("hidden-playlist");
+        moodBox.classList.add("hidden-box");
+    });
+
+    moodSelector7.addEventListener("click", function() {
+        embeddedPlaylist.classList.remove("hidden-playlist");
+        moodBox.classList.add("hidden-box");
+    });
+
+
+/* Let's rewrite this! Made absolutely no sense lol.
     function getLinkToEmbed() {
         let displayLink = "";
         let moodSelectorA = document.querySelector(".mood-selector-a");
@@ -56,4 +109,35 @@ document.addEventListener("DOMContentLoaded", function() {
         });
         return displayLink;
     }
+*/
+/*    function getLinkToEmbed() {
+
+        var displayLink = "";
+        if (moodSelector1.classList.contains("clicked")) {
+            displayLink = "https://open.spotify.com/embed/playlist/4CjEUUoSBF5zdEA5jfJUNZ?utm_source=generator";
+        }
+        else if (moodSelector2.classList.contains("clicked")) {
+            displayLink = "https://open.spotify.com/embed/playlist/2h3USsPOMXeNuz5lU4Rsdn?utm_source=generator";
+        }
+        else if (moodSelector3.classList.contains("clicked")) {
+            displayLink = "https://open.spotify.com/embed/playlist/05PtFCMUzNfznlZDxdepM1?utm_source=generator";
+        }
+        else if (moodSelector4.classList.contains("clicked")) {
+            displayLink = "https://open.spotify.com/embed/playlist/3IFgoC61KWrfTN8O7O79AG?utm_source=generator";
+        }
+        else if (moodSelector5.classList.contains("clicked")) {
+            displayLink = "https://open.spotify.com/embed/playlist/4aD5qAacFjB6jGwmPdJb8P?utm_source=generator";
+        }
+        else if (moodSelector6.classList.contains("clicked")) {
+            displayLink = "https://open.spotify.com/embed/playlist/3AQM3CEAlqK96YCohnTAcy?utm_source=generator";
+        }
+        else if (moodSelector7.classList.contains("clicked")) {
+            displayLink = "https://open.spotify.com/embed/playlist/1atBiiQZQLGS6Sz6Hb5Veh?utm_source=generator";
+        }
+        console.log(displayLink);
+        return displayLink;
+    }
+
+    document.getElementById("playlistLink").src = getLinkToEmbed();
+*/
 });
