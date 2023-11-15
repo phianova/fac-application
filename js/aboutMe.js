54d5b24470a8ffcf28ed644e5bfbc879677f4ba3
phianova <98988580+phianova@users.noreply.github.com>
2023-11-15 15:16:40 +0000

    Randomised video link and setup for dice roller

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -98,13 +98,56 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("playlistLink").onload = function () {
         loadingScreen.classList.add("hidden-loading-screen");
     }
+////////////////////////////
+/* Dice roller! */
+////////////////////////////
+
+    //Declarations
+    let diceModalContent = document.querySelector(".dice-modal-content");
+    let openDiceModal = document.querySelector(".open-dice-modal");
+    let closeDiceModal = document.querySelector(".close-dice-modal");
+    let diceText = document.querySelector(".dice-text");
+
+// Control modal opening, closing and background blurring
+    openDiceModal.addEventListener("click", function() {
+        diceModalContent.classList.remove("hidden-modal");
+        blurBackground.classList.remove("hidden-blur");
+        diceText.classList.remove("hidden-dice-text");
+    });
+
+    let closeDiceModalFunction = function() {
+        diceModalContent.classList.add("hidden-modal");
+        blurBackground.classList.add("hidden-blur");
+    };
+
+    blurBackground.addEventListener("click", closeDiceModalFunction);
+    closeDiceModal.addEventListener("click", closeDiceModalFunction);
+
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape" && !dicemodalContent.classList.contains("hidden")) {
+            closeDiceModalFunction();
+        }
+    });
+
+
 
 });
 
 ////////////////////////////
 /* Randomised video link! */
 ////////////////////////////
-
+function randomVideo () {
+    const linksArray = [
+        "https://youtu.be/qXCy-_DcKuE",
+        "https://youtu.be/iZWLDkuGDas",
+        "https://youtu.be/pkpmTEnaHkA",
+        "https://youtu.be/A34Iz8z3Tks",
+        "https://youtu.be/AdtW3DKy4jk"
+    ]
+    let videoLink = document.getElementById("video-link");
+    videoLink.videoSelector = Math.floor(Math.random()*5);
+    videoLink.href = linksArray[videoLink.videoSelector];
+}
 
 ////////////////////////////
 /* Dice roller */
