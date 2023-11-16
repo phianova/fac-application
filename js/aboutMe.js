e79f9b1e6ebd2b26aae9a536907a87ca351db31c
phianova <98988580+phianova@users.noreply.github.com>
2023-11-16 01:57:03 +0000

    Dice roller, travel pics, linked pics

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -99,20 +99,20 @@ document.addEventListener("DOMContentLoaded", function() {
         loadingScreen.classList.add("hidden-loading-screen");
     }
 ////////////////////////////
-/* Dice roller! */
+/* Dice roller modal */
 ////////////////////////////
 
     //Declarations
     let diceModalContent = document.querySelector(".dice-modal-content");
     let openDiceModal = document.querySelector(".open-dice-modal");
     let closeDiceModal = document.querySelector(".close-dice-modal");
-    let diceText = document.querySelector(".dice-text");
+    let diceTextElement = document.querySelector(".dice-text");
 
 // Control modal opening, closing and background blurring
     openDiceModal.addEventListener("click", function() {
         diceModalContent.classList.remove("hidden-modal");
         blurBackground.classList.remove("hidden-blur");
-        diceText.classList.remove("hidden-dice-text");
+        diceTextElement.classList.remove("hidden-dice-text");
     });
 
     let closeDiceModalFunction = function() {
@@ -134,6 +134,137 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 ////////////////////////////
+/* Dice roller code */
+////////////////////////////
+
+const diceArray = [
+    [1, "Oof! Critical failure. ", "./images/diceRoll/1.png"],
+    [2, "Aw, that's pretty low. No dice, I'm afraid! (Well, one die..) ", "./images/diceRoll/2.png"],
+    [3, "Aw, that's pretty low. No dice, I'm afraid! (Well, one die..) ", "./images/diceRoll/3.png"],
+    [4, "Aw, that's pretty low. No dice, I'm afraid! (Well, one die..) ", "./images/diceRoll/4.png"],
+    [5, "Aw, that's pretty low. No dice, I'm afraid! (Well, one die..) ", "./images/diceRoll/5.png"],
+    [6, "Aw, that's pretty low. No dice, I'm afraid! (Well, one die..) ", "./images/diceRoll/6.png"],
+    [7, "Aw, that's pretty low. No dice, I'm afraid! (Well, one die..) ", "./images/diceRoll/7.png"],
+    [8, "Ooooh, nearly a success...but not quite. ", "./images/diceRoll/8.png"],
+    [9, "SO close! But not quite. ", "./images/diceRoll/9.png"],
+    [10, "YES! You JUST made it. ", "./images/diceRoll/10.png"],
+    [11, "Nice one! Success. ", "./images/diceRoll/11.png"],
+    [12, "Nice one! Success. ", "./images/diceRoll/12.png"],
+    [13, "Nice one! Success. ", "./images/diceRoll/13.png"],
+    [14, "Nice one! Success. ", "./images/diceRoll/14.png"],
+    [15, "Nice one! Success. ", "./images/diceRoll/15.png"],
+    [16, "Nice one! Success. ", "./images/diceRoll/16.png"],
+    [17, "Nice one! Success. ", "./images/diceRoll/17.png"],
+    [18, "Wowwww you're doing great!", "./images/diceRoll/18.png"],
+    [19, "Well done! A roaring success!", "./images/diceRoll/19.png"],
+    [20, "PEW PEW PEW! Critical hit.", "./images/diceRoll/20.png"]
+]
+
+var adventureStage = 0;
+var adventureStageText = "You're in a forest, looking for birds and mushrooms. You see a nest up a tree. Roll Strength to climb the tree to take a picture (without disturbing the birds, of course)."
+
+function restartDiceRoll() {
+    let diceRoller = document.getElementById("dice-roller");
+    diceRoller.style.display = "inline-flex";
+    let restartDiceRoller = document.getElementById("restart-dice-roller");
+    restartDiceRoller.style.display = "none";
+
+    adventureStage = 0;
+    adventureStageText = "You're in a forest, looking for birds and mushrooms. You see a nest up a tree. Roll Strength to climb the tree to take a picture (without disturbing the birds of course)."
+    let diceText = document.getElementById("dice-text");
+    diceText.textContent = adventureStageText;
+    let diceImage = document.getElementById("dice-image");
+    diceImage.src = "./images/diceRoll/questionMark.png"
+}
+
+var finished = false;
+
+function diceRoll() {
+    finished = false;
+    diceSelector = Math.floor(Math.random()*20);
+    diceResult = diceSelector+1;
+    if (diceResult >= 10) {
+        adventureStage++;
+    }
+    adventureStage++;
+    adventureProgress();
+    if (finished == true) {
+        let diceRoller = document.getElementById("dice-roller");
+        diceRoller.style.display = "none";
+        let restartDiceRoller = document.getElementById("restart-dice-roller");
+        restartDiceRoller.style.display = "inline-flex";
+    }
+    generateDiceText();
+    generateImage();
+}
+
+function adventureProgress() {
+    const adventureStageArray = [
+        "You're in a forest, looking for birds and mushrooms. You see a nest up a tree. Roll Strength to climb the tree to take a picture (without disturbing the birds of course).",
+        "You fell down onto the forest floor. But whoa...there are some really cool mushrooms down here. Roll Intelligence to see if you can identify any.",
+        "You climbed the tree and found some adorable baby birds. Can you identify them? Roll Intelligence...",
+        "The birds are cute and fluffy but you have no clue what they are. Buuuut there are some real cool mushrooms down here. Roll Intelligence to see if you can find an edible one.",
+        "You identified the birds and climbed back down the tree to safety. But wait! There are some cool mushrooms down here. Can you identify those too?",
+        "Ummmm...maybe this one? It's bright and fun looking! Probably means it's edible right? You light your campfire and start making a mushroom soup. Roll Constitution to see if you can stomach it...",
+        "C'est un chanterelle, non? That's gotta be a good'un. You light your campfire and make a soup. Roll Constitution to see how it goes down...",
+        "That was not good soup. Looks like you're going home with a tummy ache...Roll to start again!",
+        "Dammmmn that's good soup. You go home happy and contented with a great day's foraging. Roll to explore again!"
+    ]
+    //Stage 0: tree climb question
+    //Stage 1: tree climb failed - skip bird stages entirely, go to stage 5/6.
+    //Stage 2: tree climb succeeded - go to stage 3/4.
+    //Stage 3: bird ID failed - go to stage 5/6.
+    //Stage 4: bird ID succeeded - go to stage 5/6.
+    //Stage 5: mushroom ID failed - go to stage 7/8.
+    //Stage 6: mushroom ID succeeded - go to stage 7/8.
+    //Stage 7: bad soup, restart
+    //Stage 8: bad soup, restart
+    finished = false;
+    console.log("before", adventureStage);
+    adventureStageText=adventureStageArray[adventureStage];
+    if (adventureStage == 1) { //If first roll fails
+        adventureStage = 4;
+    }
+    /*else if (adventureStage == 2) { //If first roll succeeds
+        adventureStageText = adventureStageArray[2];
+    }*/
+    else if (adventureStage == 3) { //If first roll succeeds and second roll fails
+        adventureStageText = adventureStageArray[3];
+        adventureStage = 4;
+    }
+    /*else if (adventureStage == 4) { //If first and second roll succeed
+        adventureStageText = adventureStageArray[4];
+    }*/
+    else if (adventureStage == 5) { //mushroom identification fails
+        //text
+        adventureStage = 6;
+    }
+    /*else if (adventureStage == 6) { //mushrom ID succeeds
+        text
+    }*/
+    else if (adventureStage == 7) { //soup fails
+        finished = true;    
+    }
+    else if (adventureStage == 8) {
+        finished = true;
+    }
+    console.log("after", adventureStage, finished);
+}
+
+function generateDiceText() {
+    let diceReaction = diceArray[diceSelector][1];
+    let diceText = document.getElementById("dice-text");
+    diceText.textContent = diceReaction+adventureStageText;
+}
+
+function generateImage() {
+    let diceImage = document.getElementById("dice-image");
+    diceImage.src = diceArray[diceSelector][2];
+    let restartDiceImage = document.getElementById("restart-dice-image");
+    restartDiceImage.src = diceImage.src;
+}
+
+////////////////////////////
 /* Randomised video link! */
 ////////////////////////////
 function randomVideo () {
@@ -148,8 +279,3 @@ function randomVideo () {
     videoLink.videoSelector = Math.floor(Math.random()*5);
     videoLink.href = linksArray[videoLink.videoSelector];
 }
-
-////////////////////////////
-/* Dice roller */
-////////////////////////////
-
